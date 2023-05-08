289a33bb5b38c76574aa3f566970bb6b94668e29
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-05-08 12:19:35 +0200

    micronaut rapid metrics

--- hm-rapids-and-rivers-v2-micronaut/src/main/kotlin/no/nav/hm/rapids_rivers/micronaut/RapidsRiversFactory.kt
+++ hm-rapids-and-rivers-v2-micronaut/src/main/kotlin/no/nav/hm/rapids_rivers/micronaut/RapidsRiversFactory.kt
@@ -1,12 +1,15 @@
 
 package no.nav.hm.rapids_rivers.micronaut
 
+import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Primary
 import io.micronaut.context.annotation.Requires
 import io.micronaut.context.env.Environment
 import jakarta.inject.Singleton
 import no.nav.helse.rapids_rivers.KafkaConfig
 import no.nav.helse.rapids_rivers.KafkaRapid
+import no.nav.helse.rapids_rivers.KafkaRapidMetrics
 
 @Factory
 @Requires(property = "rapidsandrivers.enabled", notEquals="false", defaultValue = "true")
@@ -24,5 +27,10 @@ class RapidsRiversFactory {
             keystorePassword = kafkaProps.keystorePassword)
         return KafkaRapid.create(kafkaConfig, kafkaProps.topic, kafkaProps.extraTopics)
     }
+
+    @Bean
+    @Singleton
+    @Primary
+    fun rapidMetrics(kafkaRapid: KafkaRapid): KafkaRapidMetrics = KafkaRapidMetrics(kafkaRapid)
 
 }
