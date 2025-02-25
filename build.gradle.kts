36d9ac42ffa096f0b964afc9e7bc20335feb8587
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-02-25 09:18:01 +0100

    removed KTOR, we don't use it

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.21"
     kotlin("kapt") version "1.9.21"
+    id("com.github.ben-manes.versions") version "0.47.0"
     id("java")
     id("maven-publish")
 }
